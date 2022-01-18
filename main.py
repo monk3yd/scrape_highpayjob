14aacbaff15864530b9bda28c18ff0e3f67155de
monk3yd <jegajardog@gmail.com>
2022-01-17 23:33:31 -0300

    Works with all pages & Save to CSV.

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
 import requests
 import csv
 
+from random import random
+from time import sleep
 from bs4 import BeautifulSoup
 from pprint import pprint
 
-# for page in range():
-# Parse URL Page
-# https://www.payscale.com/college-salary-report/majors-that-pay-you-back/bachelors/page/2
-URL = "https://www.payscale.com/college-salary-report/majors-that-pay-you-back/bachelors/page/1"
-response = requests.get(URL)
-html = response.text
-soup = BeautifulSoup(html, "html.parser")
-# print(soup.prettify())
-
+# General Variables
 rows = []
+table_headings_created = False
 
-# Scrape table headings
-table_head = soup.select_one(selector=".data-table thead tr")
-headings = [header.getText() for header in table_head]
-rows.append(headings)
+# Iterate through pages
+for current_page in range(1, 35):
+    # Parse URL Page
+    URL = f"https://www.payscale.com/college-salary-report/majors-that-pay-you-back/bachelors/page/{current_page}"
+    print(f"Current Page: {current_page}/34")
+    response = requests.get(URL)
+    html = response.text
+    soup = BeautifulSoup(html, "lxml")
 
-# Scrape table data
-table_cell = soup.select(selector=".data-table__cell")
-i = 0
-row = []
-for data in table_cell:
-    if i > 5:
-        rows.append(row)
-        row = []
-        i = 0
-    filtered_data = data.getText().split(":")[1]
-    row.append(filtered_data)
-    i += 1
+    if not table_headings_created:
+        # Scrape table headings
+        table_head = soup.select_one(selector=".data-table thead tr")
+        headings = [header.getText() for header in table_head]
+        rows.append(headings)
+        table_headings_created = True
+        print("Table Headings Created...")
+
+    # Scrape table data
+    table_cell = soup.select(selector=".data-table__cell")
+    i = 0
+    row = []
+    for data in table_cell:
+        if i > 5:
+            rows.append(row)
+            row = []
+            i = 0
+        filtered_data = data.getText().split(":")[1]
+        row.append(filtered_data)
+        i += 1
+
+    sleep(random())
 # print(rows)
 
 # Create CSV
